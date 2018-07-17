c1012e8342a695b332b2d174b15482b2e5a30cb8
Codeiffor <sid96@protonmail.com>
2018-07-17 08:12:19 +0530

    UI Complete

--- main.js
+++ main.js
@@ -7,18 +7,65 @@ var listGroupItem;
 
 var resp,innerHtml,theUrl,xmlHttp,inputCity,inputCityLength,i,j;
 //location names
-var locationNames=["London","San Francisco","New York","Sydney","Philadelphia","Manchester","Houston","Birmingham","Los Angeles","Berlin","Chicago","Tokyo","Beijing","Mexico City","Osaka","Bristol","Mumbai","Glasgow","Amsterdam","Johannesburg","Rio de Janeiro","Shanghai","New Delhi","Jakarta","Seoul","São Paulo","Moscow","Hong Kong","San Diego","San Jose","Dallas","Indianapolis","San Antonio","Montréal","Jacksonville","Austin","Kiev","Phoenix","Madrid","Toronto","Fort Worth","Guangzhou","Columbus","Memphis","Washington DC","Paris","Munich","Brisbane","Rome","Milwaukee","Bradford","Liverpool","Wakefield","Istanbul","El Paso","Seattle","Baltimore","Las Vegas","Melbourne","Buenos Aires","Boston","Nashville","Edinburgh","Perth","Karachi","Hamburg","Dubai","Leeds","Sheffield","Adelaide","Charlotte","Detroit","Louisville","Barcelona","Denver","Portland","Oklahoma City","Cardiff","Kolkata","Cairo","Virginia Beach","Kinshasa","Honolulu","Tehrān","Colorado Springs","Auckland","Dongguan","Sacramento","Kansas City","Fresno","Bangkok","Ho Chi Minh City","Mesa","Atlanta","Bangalore","Lima","Albuquerque","Tucson","Lagos","Long Beach","Shenzhen","Bogotá","Omaha","Raleigh","Miami","Exeter","Hà Nội","Casablanca","Singapore","Yokohama","Nairobi","Tianjin","Dhaka","Pyongyang","Addis Ababa","Hyderabad","Santa Cruz","Dublin","Budapest","Milan","Cambridge","Oxford","Vienna","Brussels","Cologne","Riyadh","Damascus","Ankara","Little Rock","Wilmington","Santiago","Birmingham","Baghdad","Boise","Anchorage","Athens","Santorini","Reykjavík","Sofia","Prague","Zagreb","Oslo","Copenhagen","Bucharest","Torino","Stockholm","Naples","Warsaw","Bridgeport","Wichita","Richmond","New Orleans","Calgary","Edmonton","St Petersburg","Manila","Vancouver","Maracaibo","Caracas","Cheyenne","Charleston","Santander","İzmir","Toulouse","Bordeaux","Wuhan","Marseille","Lille","Ahmedabad","Lyon","Nice","Lahore","Belfast","Bremen","Stoke-on-Trent","Fargo","Sendai","Truro","Preston","Sunderland","Lisbon","Phuket","Palm Springs","Leicester","Stuttgart","Coventry","Gothenburg","Hanover","St. Louis","Salvador","Plymouth","Lake Tahoe","Nuremberg","Mountain View","Kawasaki","Kyoto","Kobe","Hangzhou","Blackpool","Yangon","Bakersfield","Salt Lake City","Geneva","Portland","Reading","Durban","Saitama","Brighton","Dresden","Ajaccio","Pune","Mombasa","Providence","Chennai","Kharkiv","Helsinki","Taipei","Essen","St Ives","Aberdeen","Oakland","Sapporo","Ipswich","Norwich","Christchurch","Surat","Busan","Manchester","Hiroshima","Northampton","Leipzig","Southend-on-Sea","The Hague","Minsk","Salford","Kirkwall","Swansea","Penzance","Ibadan","Billings","Alexandria","Newcastle","Jackson","Sioux Falls","Nagoya","Swindon","Brasília","Dundee","Kano","Kitakyushu","Denpasar","Boulder","Minneapolis","Frankfurt","Falmouth","Middlesbrough","Rhyl","Bournemouth","Fukuoka","Newark","Manukau","Luton","Wellington","Abidjan","Kingston upon Hull","Des Moines","Windhoek","Calvi","Venice","Santa Cruz de Tenerife","Chengdu","York","Kuala Lumpur","Portsmouth","Burlington","Santa Fe","Zurich","Nottingham","Derby","Huddersfield","Dortmund","Wolverhampton","Cape Town","Düsseldorf","Columbia"]
+var locationNames=["London","San Francisco","New York","Sydney","Philadelphia","Manchester","Houston","Birmingham","Los Angeles","Berlin","Chicago","Tokyo","Beijing","Mexico City","Osaka","Bristol","Mumbai","Glasgow","Amsterdam","Johannesburg","Rio de Janeiro","Shanghai","New Delhi","Delhi","Jakarta","Seoul","São Paulo","Moscow","Hong Kong","San Diego","San Jose","Dallas","Indianapolis","San Antonio","Montréal","Jacksonville","Austin","Kiev","Phoenix","Madrid","Toronto","Fort Worth","Guangzhou","Columbus","Memphis","Washington DC","Paris","Munich","Brisbane","Rome","Milwaukee","Bradford","Liverpool","Wakefield","Istanbul","El Paso","Seattle","Baltimore","Las Vegas","Melbourne","Buenos Aires","Boston","Nashville","Edinburgh","Perth","Karachi","Hamburg","Dubai","Leeds","Sheffield","Adelaide","Charlotte","Detroit","Louisville","Barcelona","Denver","Portland","Oklahoma City","Cardiff","Kolkata","Cairo","Virginia Beach","Kinshasa","Honolulu","Tehrān","Colorado Springs","Auckland","Dongguan","Sacramento","Kansas City","Fresno","Bangkok","Ho Chi Minh City","Mesa","Atlanta","Bangalore","Lima","Albuquerque","Tucson","Lagos","Long Beach","Shenzhen","Bogotá","Omaha","Raleigh","Miami","Exeter","Hà Nội","Casablanca","Singapore","Yokohama","Nairobi","Tianjin","Dhaka","Pyongyang","Addis Ababa","Hyderabad","Santa Cruz","Dublin","Budapest","Milan","Cambridge","Oxford","Vienna","Brussels","Cologne","Riyadh","Damascus","Ankara","Little Rock","Wilmington","Santiago","Birmingham","Baghdad","Boise","Anchorage","Athens","Santorini","Reykjavík","Sofia","Prague","Zagreb","Oslo","Copenhagen","Bucharest","Torino","Stockholm","Naples","Warsaw","Bridgeport","Wichita","Richmond","New Orleans","Calgary","Edmonton","St Petersburg","Manila","Vancouver","Maracaibo","Caracas","Cheyenne","Charleston","Santander","İzmir","Toulouse","Bordeaux","Wuhan","Marseille","Lille","Ahmedabad","Lyon","Nice","Lahore","Belfast","Bremen","Stoke-on-Trent","Fargo","Sendai","Truro","Preston","Sunderland","Lisbon","Phuket","Palm Springs","Leicester","Stuttgart","Coventry","Gothenburg","Hanover","St. Louis","Salvador","Plymouth","Lake Tahoe","Nuremberg","Mountain View","Kawasaki","Kyoto","Kobe","Hangzhou","Blackpool","Yangon","Bakersfield","Salt Lake City","Geneva","Portland","Reading","Durban","Saitama","Brighton","Dresden","Ajaccio","Pune","Mombasa","Providence","Chennai","Kharkiv","Helsinki","Taipei","Essen","St Ives","Aberdeen","Oakland","Sapporo","Ipswich","Norwich","Christchurch","Surat","Busan","Manchester","Hiroshima","Northampton","Leipzig","Southend-on-Sea","The Hague","Minsk","Salford","Kirkwall","Swansea","Penzance","Ibadan","Billings","Alexandria","Newcastle","Jackson","Sioux Falls","Nagoya","Swindon","Brasília","Dundee","Kano","Kitakyushu","Denpasar","Boulder","Minneapolis","Frankfurt","Falmouth","Middlesbrough","Rhyl","Bournemouth","Fukuoka","Newark","Manukau","Luton","Wellington","Abidjan","Kingston upon Hull","Des Moines","Windhoek","Calvi","Venice","Santa Cruz de Tenerife","Chengdu","York","Kuala Lumpur","Portsmouth","Burlington","Santa Fe","Zurich","Nottingham","Derby","Huddersfield","Dortmund","Wolverhampton","Cape Town","Düsseldorf","Columbia"]
 var locationNamesLength=locationNames.length;
-
+var months=['Jan','Feb','Mar','April','May','June','July','Aug','Sept','Oct','Nov','Dec'];
 
 //keydown listener on input
 city.addEventListener('keydown',function(event){
   //timeout for input to read latest value
   window.setTimeout(function(){
+    if(event.key=='Enter'){
+      j=-1;
+      for(i=0;i<listGroupItem.length;i++){
+        if(listGroupItem[i].classList.contains('bg1')){
+          j=i;
+          break; 
+        }
+      }
+      if(j!=-1){
+        city.value=listGroupItem[j].textContent;
+      }
+      getWeather.click();
+      return;
+    }
+
+    //up-down arrow movement on list
+    if(event.key=='ArrowDown'){
+      j=-1;
+      for(i=0;i<listGroupItem.length;i++){
+        if(listGroupItem[i].classList.contains('bg1')){
+          j=i;
+          break; 
+        }
+      }
+      if(j!=listGroupItem.length-1){
+        if(j!=-1)
+          listGroupItem[j].classList.remove('bg1');
+          listGroupItem[j+1].className+=' bg1';
+      }
+      return;
+    }
+    if(event.key=='ArrowUp'){
+      j=-1;
+      for(i=0;i<listGroupItem.length;i++){
+        if(listGroupItem[i].classList.contains('bg1')){
+          j=i;
+          break; 
+        }
+      }
+      if(j>0){
+        listGroupItem[j].classList.remove('bg1');
+        listGroupItem[j-1].className+=' bg1';
+      }
+      return;
+    }
+
+    //creating list
     list.innerHTML='<ul class="list-group">';
     inputCity=city.value;
     inputCityLength=inputCity.length;
-    //matching input with city names
+      //matching input with city names
     if(inputCityLength){
       j=0;
       for(i=0;i<locationNamesLength;i++){
@@ -30,10 +77,14 @@ city.addEventListener('keydown',function(event){
       }
     }
     list.innerHTML+='</ul>';
+
     //mouse events on list
     listGroupItem=document.querySelectorAll('.list-group-item');
     for(i=0;i<listGroupItem.length;i++){
-      listGroupItem[i].addEventListener('mouseenter',function(event){
+      listGroupItem[i].addEventListener('mousemove',function(event){
+        for(j=0;j<listGroupItem.length;j++){
+          listGroupItem[j].classList.remove('bg1');
+        }
         this.className+=' bg1';
       });
       listGroupItem[i].addEventListener('mouseleave',function(event){
@@ -55,10 +106,12 @@ city.focus();
 
 //click listener on button
 getWeather.addEventListener('click',function(event){
+  if(city.value=='')return;
   list.innerHTML='';
   getXhr();
+  weatherResponse.innerHTML='Getting Weather Data for '+city.value;
   for(var i=0;i<6;i++){
-    predictionWindow[i].innerHTML='<img class="my-4 mx-auto" src="img/loading.svg" alt="Getting Weather...">';
+    predictionWindow[i].innerHTML='<img class="my-5 py-3 mx-auto" src="img/loading.svg" alt="Getting Weather...">';
   }
 });
 //function interacting with server
@@ -66,18 +119,39 @@ function getXhr(){
   theUrl='http://localhost:3001/'+city.value;
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", theUrl,true);
-  xmlHttp.send( null );
-  xmlHttp.onreadystatechange = function() {
-    resp=xmlHttp.responseText;
-    console.log(resp);    
-    resp=JSON.parse(resp);
-    weatherResponse.innerHTML='<h3>Weather of '+resp['title']+'</h3>';
-    for(var i=0;i<6;i++){
-      innerHtml='<div class="my-1">Weather for = '+resp['consolidated_weather'][i]['applicable_date']+
-      '<br>Max Temperature = '+Math.round(resp['consolidated_weather'][i]['max_temp'])+
-      '°C<br>Min Temperature = '+Math.round(resp['consolidated_weather'][i]['min_temp'])+
-      '°C<br>State = '+resp['consolidated_weather'][i]['weather_state_name']+'</div';
-      predictionWindow[i].innerHTML=innerHtml;
+  xmlHttp.onreadystatechange=function(){
+    if(xmlHttp.readyState==4){
+      resp=xmlHttp.responseText;
+      resp=JSON.parse(resp);
+      console.log(xmlHttp.status); 
+      weatherResponse.innerHTML='Weather at '+resp['title'];
+      for(var i=0;i<6;i++){
+        var date=resp["consolidated_weather"][i]["applicable_date"];
+        var day=months[parseInt(date.slice(5,7))-1]+' '+date.slice(8,10);
+        if(i==0)day='Today';
+        if(i==1)day='Tomorrow';
+        innerHtml='<div style="font-size: 1.3em;font-family:"Courier New", Courier, monospace;font-weight: 600;color:palegreen">'
+        +day
+        +'</div><div><img src="/img/'
+        +resp["consolidated_weather"][i]["weather_state_abbr"]
+        +'.png"height="40px"></div><div style="font-size: 0.9em">'
+        +resp["consolidated_weather"][i]["weather_state_name"]
+        +'</div><div style="font-size: 1.5em;color:paleturquoise;font-weight: 700;margin-top:0">'
+        +Math.round(resp["consolidated_weather"][i]["max_temp"])
+        +'°C</div><div style="font-size: 1.5em;color:paleturquoise;font-weight: 700;margin-top:0">'
+        +Math.round(resp["consolidated_weather"][i]["min_temp"])
+        +'°C</div><div>Wind : '
+        +Math.round(resp["consolidated_weather"][i]["wind_speed"])
+        +'mph <img height="16px"style="transform:rotate('
+        +Math.round(resp["consolidated_weather"][i]["wind_direction"])
+        +'deg)"src="img/down-arrow.png"></div><div>Humidity : '
+        +resp["consolidated_weather"][i]["humidity"]
+        +'%</div>';
+        predictionWindow[i].innerHTML=innerHtml;
+      }
+      city.value='';
+      city.focus();
     }
   }
+  xmlHttp.send( null );
 }
